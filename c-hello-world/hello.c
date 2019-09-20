9dab613a4d73744339b53cf3ef28d25975a859ff
Jason Andrews <jason.r.andrews@comcast.net>
2019-09-20 18:16:02 +0100

    Simple multi-architecture C example for Docker

--- c-hello-world/hello.c
+++ c-hello-world/hello.c
@@ -7,6 +7,18 @@
  
 int main() 
 {
-    printf("Hello, my architecture is %s\n", ARCH);
+    printf("Hello, architecture from uname is %s\n", ARCH);
+
+    switch (sizeof(void *))
+    {
+        case 4:
+            printf("32-bit machine\n");
+            break;
+        case 8:
+            printf("64-bit machine\n");
+            break;
+        default:
+            printf("unknown machine\n");
+    }
     exit(0);
 }
