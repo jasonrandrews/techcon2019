ba2c288b67811400b83977e6b09363b87d60bab5
Jason Andrews <jason.r.andrews@comcast.net>
2019-09-22 18:41:58 -0500

    Simple multi-architecture C example for Docker

--- c-hello-world/aws-build.ps1
+++ c-hello-world/aws-build.ps1
@@ -1,6 +1,6 @@
 # enter the IP address of an AWS A1 instance 
 # Make sure to change the tag name of the image to your hub account
 
-docker buildx create --use --platform linux/arm/v7,linux/arm64 --name aws-builder  ubuntu@50.17.107.82
+docker buildx create --use --platform linux/arm/v7,linux/arm64 --name aws-builder  ubuntu@100.26.131.243
 docker buildx use aws-builder
-docker buildx build --platform linux/arm64,linux/arm/v7 -t jasonrandrews/c-hello-world --push .
+docker buildx build --platform linux/arm64,linux/arm/v7 -t jasonrandrews/c-hello-world-a1 --push .
---
+++ c-hello-world/native-run.ps1
@@ -0,0 +1,6 @@
+# docker run will automatically get and run the image based on the current machine
+
+# uncomment 1 line to select the architectures
+
+docker run --rm -it jasonrandrews/c-hello-world
+#docker run --rm -it jasonrandrews/c-hello-world-a1
